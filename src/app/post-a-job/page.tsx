ad500b02c6e0bb01a96a977962c562de17afe254
Devtaaa <devtadanarsa69@gmail.com>
2024-02-04 23:29:43 +0800

    creating post job form schema

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -1,6 +1,21 @@
+import { jobFormSchema } from '@/lib/form-schema'
+import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from "zod";
 
 const PostJobPage = () => {
+    const form = useForm<z.infer<typeof jobFormSchema>>({
+        resolver: zodResolver(jobFormSchema),
+        defaultValues: {
+            requiredSkills: []
+        }
+    })
+
+    const onSubmit = (values: z.infer<typeof jobFormSchema>) => {
+        console.log(values)
+    }
+    
   return (
     <div>PostJobPage</div>
   )
